094e471f0d83f2123c52a72efd808d5921676c15
TaiwanCCyoyo <xjp0134323@gmail.com>
2023-05-14 17:40:06 +0800

    * temp commit

--- pandas_datareader_test.py
+++ pandas_datareader_test.py
@@ -2,12 +2,22 @@
 
 import argparse
 import logging
-import twstock
+
+# basic
 import numpy as np
+import pandas as pd
+
+# get data
+import pandas_datareader as pdr
+
+# visual
 import matplotlib.pyplot as plt
-import json
-# from twstock import Stock
-# import pandas as pd
+import matplotlib as mpf
+import seaborn as sns
+
+# time
+import datetime as datetime
+
 
 args = None
 logger = None
@@ -25,8 +35,6 @@ def arg_parse():
     # 設定參數選項
     parser.add_argument('--version', action='version', version='%(prog)s 0.1')
     parser.add_argument('-v', '--verbose', action='count', default=0)
-    parser.add_argument('-u', '--update', dest='update',
-                        action='store_true', help='twstock update codes')
     parser.add_argument('-l', '--log', dest='log', type=str,
                         metavar='LOG_FILE', default="twstock_test.log", help='log file name')
 
@@ -68,40 +76,7 @@ def get_logger():
 
 
 if __name__ == '__main__':
-    args = arg_parse()  # 命令參數解析
-    logger = get_logger()  # 取得logger
+    start = datetime.datetime(2023, 4, 1)
+    end = datetime.datetime(2023, 5, 14)
 
-    # 更新股票代號
-    if (args.update):
-        logger.info("更新股票代號...")
-        twstock.__update_codes()
-        logger.info("完成")
-
-    # 取得上市櫃代號的相關資訊
-    # stock_dict = twstock.codes
-    # new_stock_dict = {}
-    # for key, value in stock_dict.items():
-    #     if (value[0] == "股票"):
-    #         new_stock_dict[key] = value
-
-    # stock_dict = new_stock_dict
-    # logger.info("所有台股資訊")
-    # logger.info(json.dumps(stock_dict, indent=4, ensure_ascii=False))
-
-    # key = '3260'
-    # stock = twstock.Stock(key)
-    # logger.info(stock.fetch_31()[0].open)
-
-
-# 获取台股所有股票资讯
-# stock_info = twstock.all
-
-# # 遍历所有股票，获取当前股票的资讯
-# for stock in stock_info:
-#     sid = stock_info[stock]['code']
-#     stock_price = twstock.realtime.get(sid)
-#     ma_5 = twstock.indicator.calculate_ma(twstock.Stock(sid).close, 5)[-1]
-
-#     # 判断当前股票是否在 5 日均线上
-#     if stock_price and float(stock_price['realtime']['latest_trade_price']) >= ma_5:
-#         print(sid, 'is on 5MA')
+    df_2330 = pdr.DataReader('2330.TW', 'yahoo', start=start)
